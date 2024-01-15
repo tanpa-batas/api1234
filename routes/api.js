e73eee6583d0386047dc604c1f3109b06e2c505d
desactive-minha <30399389-desactive-minha@users.noreply.replit.com>
2024-01-15 12:31:33 +0000

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,129 @@
+__path = process.cwd()
+
+let express = require('express'), router = express.Router(), yt = require('../lib/yt'), ytdl = require('../lib/ytdl'), ai = require('../lib/Ai'), buffer = require('../lib/function'), fs = require('fs'), spot = require('../lib/downloader'), { toAudio } = require('../lib/converter')
+
+router.get('/remini', async (req, res) => {
+  let link = req.query.link
+  let apikey = req.query.apikey
+  if (!link) return res.json(global.status.link)
+    if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let buff = await buffer.getBuffer(link)
+  let result = await ai.remini(buff)
+  await fs.writeFileSync(__path +'/tmp/remini.jpg', result)
+  await res.sendFile(__path +'/tmp/remini.jpg')
+})
+
+/*
+Ai
+*/
+router.get('/aoi', async (req, res) => {
+  let text = req.query.text
+  let apikey = req.query.apikey
+  if (!text) return res.json(global.status.text)
+    if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  ai.aoi(text).then(_ => {
+
+  fs.writeFileSync(__path + '/tmp/aoi.mp3', _)
+  })
+  res.sendFile(__path + '/tmp/aoi.mp3')
+ 
+})
+
+router.get('/botika', async (req, res) => {
+  let text = req.query.text
+  let apikey = req.query.apikey
+  if (!text) return res.json(global.status.query)
+    if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await ai.botika(text)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+router.get('/blackbox', async (req, res) => {
+  let text = req.query.text
+  let apikey = req.query.apikey
+  if (!text) return res.json(global.status.query)
+    if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await ai.blackbox(text)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+
+/*
+Downloader
+*/
+
+router.get('/spotify/search', async (req, res) => {
+  let q = req.query.q
+  let apikey = req.query.apikey
+  if (!q) return res.json(global.status.query)
+  if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await spot.searching(q)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+router.get('/spotify/dl', async (req, res) => {
+  let link = req.query.link
+  let apikey = req.query.apikey
+  if (!link) return res.json(global.status.link)
+  if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await spot.spotifydl(link)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+router.get('/video', async (req, res) => {
+  let q = req.query.q
+  let apikey = req.query.apikey
+  if (!q) return res.json(global.status.query)
+  if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await ytdl.video(q)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+router.get('/yta', async (req, res) => {
+  let url = req.query.url
+  let apikey = req.query.apikey
+  if (!url) return res.json(global.status.url)
+  if (!url.match('youtu.be') && !url.match('youtube.com')) return res.json(global.status.invalidURL)
+  if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await ytdl.ytmp3(url)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+router.get('/ytv', async (req, res) => {
+  let url = req.query.url
+  let apikey = req.query.apikey
+  if (!url) return res.json(global.status.url)
+  if (!url.match('youtu.be') && !url.match('youtube.com')) return res.json(global.status.invalidURL)
+  if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await ytdl.ytmp4(url)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+router.get('/yts', async (req, res) => {
+  let q = req.query.q
+  let apikey = req.query.apikey
+  if (!q) return res.json(handle.query)
+  if (!apikey) return res.json(global.status.apikey)
+  if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
+  let result = await yt.search(q)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+module.exports = router
